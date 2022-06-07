bf06bf94d7fb76f21a4010c5d576c9a0df1d2f05
webmaster <webmaster@onedevs.tech>
2022-06-07 09:51:25 +0200

    onedevs: money section

---
+++ money/index.php
@@ -0,0 +1,132 @@
+<html>
+
+   <head>
+      <title>OneDevs Payments</title>
+      <link rel="stylesheet" href="../css/styles.css?v=1.0">
+   </head>
+
+   <body class="money">
+
+      <div class="top">__onedevs__</div>
+      <div class="lema">Developing the Future</div>
+
+      <!-- DATE TIME -->
+
+      <div class="date">
+         <?= \date('l, j F Y G:i:s a e'); ?>
+      </div>
+
+      <div class="money">
+
+         <!-- AVAILABLE -->
+
+         <table class="available" align="center">
+            <thead>
+               <th>Available</th>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>3,906.89 €</td>
+               </tr>
+            </tbody>
+         </table>
+
+         <!-- OFFERS -->
+
+         <table class="offers" align="center">
+            <thead>
+               <tr>
+                  <th colspan="4" class="title">Work Offers</th>
+               </tr>
+               <tr>
+                  <th>Project</th>
+                  <th>From</th>
+                  <th>Offer</th>
+                  <th>Description</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
+                  <td>richi</td>
+                  <td class="money">150 €</td>
+                  <td>rediseñar y maquetar la página <a href="http://onedevs.tech/money" target="_blank">http://onedevs.tech/money</a> Ahora mismo está muy simplona, tiene que ser más llamativa y guay</td>
+               </tr>
+               <tr>
+                  <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
+                  <td>richi</td>
+                  <td class="money">100 €</td>
+                  <td>logotipo para la marca OneDevs, página principal de futura empresa</td>
+               </tr>
+               <tr>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>richi</td>
+                  <td class="money">[ negociar ]</td>
+                  <td>ocupar el hueco negro de arriba con algo chulo, por ejemplo un tribal blanco horizontal</td>
+               </tr>
+               <tr>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>richi</td>
+                  <td class="money">30 €</td>
+                  <td>dejar las banderas de los países todas con el mismo alto y ancho (12 pixeles de altura)</td>
+               </tr>
+            </tbody>
+         </table>
+
+         <!-- PAYMENTS -->
+
+         <table class="transactions" align="center">
+            <thead>
+               <tr>
+                  <th colspan="12" class="title">Payments</th>
+               </tr>
+               <tr>
+                  <th>Project</th>
+                  <th>From</th>
+                  <th>From Bank</th>
+                  <th>From IBAN</th>
+                  <th>To</th>
+                  <th>To Bank</th>
+                  <th>To IBAN</th>
+                  <th>Send</th>
+                  <th>Transaction Date</th>
+                  <th>Transaction Method</th>
+                  <th>Task / Transaction Description</th>
+                  <th></th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>richi</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td><span title="ES92 0237 6057 8091 7267 1245">*</span></td>
+                  <td>jonathan</td>
+                  <td></td>
+                  <td><span title="ES47 1465 0100 9820 3982 7652">*</span></td>
+                  <td class="money">75 €</td>
+                  <td>Monday, 6 June 2022 09:11:32 am Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td>IMAGENES EGIPCIAS</td>
+                  <td>🤟</td>
+               </tr>
+               <tr>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>richi</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td><span title="ES92 0237 6057 8091 7267 1245">*</span></td>
+                  <td>jonathan</td>
+                  <td></td>
+                  <td><span title="ES47 1465 0100 9820 3982 7652">*</span></td>
+                  <td class="money">125 €</td>
+                  <td>Monday, 6 June 2022 19:51:36 pm Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td>MENU RESPONSIVE WWW.TATTOORUBIO.COM</td>
+                  <td>🤟</td>
+               </tr>
+            </tbody>
+         </table>
+      </div>
+   </body>
+
+</html>
