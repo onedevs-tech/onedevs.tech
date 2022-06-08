4305e5445763f277f5320a1b88e0f08cf4665892
webmaster <webmaster@onedevs.tech>
2022-06-08 18:09:03 +0200

    update logo y lista pagos

--- public/taskpay/index.php
+++ public/taskpay/index.php
@@ -8,7 +8,9 @@
    <body class="money">
 
       <center>
-         <img src="../images/logo.png" alt="logo">
+         <img src="../images/logo-solo-circulo.svg" width="200" alt="logo">
+         <div class="nasalized" style="font-size: 1.2em;">__ OneDevs __</div>
+         <div class="nasalized" style="font-size: 0.5em;">Developing The Future</div>
       </center>
 
       <!-- DATE TIME -->
@@ -27,7 +29,7 @@
             </thead>
             <tbody>
                <tr>
-                  <td>3.650,52 €</td>
+                  <td>3.936,54 €</td>
                </tr>
             </tbody>
          </table>
@@ -48,22 +50,10 @@
             </thead>
             <tbody>
                <tr>
-                  <td>Ricardo Ruiz</td>
+                  <td>Ricardo Ruiz Martínez</td>
                   <td class="money">175 €</td>
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td>Rediseñar y maquetar la página <a href="http://onedevs.tech/taskpay/" target="_blank">http://onedevs.tech/taskpay/</a> Ahora mismo está muy simplona, tiene que ser más llamativa y guay.<br />Las filas se deben poder ordenar haciendo click en el nombre de la columna.</td>
-               </tr>
-               <tr>
-                  <td>Ricardo Ruiz</td>
-                  <td class="money">75 €</td>
-                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
-                  <td>Cambiar el tribal de arriba, por uno que sea uno más fino, el que hay actualmente es un poco sobrecargado (debe seguir siendo blanco).</td>
-               </tr>
-               <tr>
-                  <td>Ricardo Ruiz</td>
-                  <td class="money">25 €</td>
-                  <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
-                  <td>El logo de OneDevs tiene la f de "future" en minúsculas. Me gustaría que fuese con mayúsculas, así: "Developing The Future".</td>
                </tr>
             </tbody>
          </table>
@@ -73,13 +63,15 @@
          <table class="transactions" align="center">
             <thead>
                <tr>
-                  <th colspan="12" class="title">Payments</th>
+                  <th colspan="14" class="title">Payments</th>
                </tr>
                <tr>
                   <th>From</th>
+                  <th>From Doc ID</th>
                   <th>From Bank</th>
                   <th>From IBAN</th>
                   <th>To</th>
+                  <th>To Doc ID</th>
                   <th>To Bank</th>
                   <th>To IBAN</th>
                   <th>Value</th>
@@ -92,11 +84,13 @@
             </thead>
             <tbody>
                <tr>
-                  <td>Ricardo Ruiz</td>
+                  <td>Ricardo Ruiz Martínez</td>
+                  <td>44365676X</td>
                   <td>Cajasur Kutxabank</td>
                   <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
                   <td>*</td>
+                  <td>ING</td>
                   <td>*</td>
                   <td class="money">75 €</td>
                   <td>Monday, 6 June 2022 09:11:32 am Europe/Madrid</td>
@@ -106,11 +100,13 @@
                   <td class="completed">🤟</td>
                </tr>
                <tr>
-                  <td>Ricardo Ruiz</td>
+                  <td>Ricardo Ruiz Martínez</td>
+                  <td>44365676X</td>
                   <td>Cajasur Kutxabank</td>
                   <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
                   <td>*</td>
+                  <td>ING</td>
                   <td>*</td>
                   <td class="money">125 €</td>
                   <td>Monday, 6 June 2022 19:51:36 pm Europe/Madrid</td>
@@ -120,11 +116,13 @@
                   <td class="completed">🤟</td>
                </tr>
                <tr>
-                  <td>Ricardo Ruiz</td>
+                  <td>Ricardo Ruiz Martínez</td>
+                  <td>44365676X</td>
                   <td>Cajasur Kutxabank</td>
                   <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
                   <td>*</td>
+                  <td>ING</td>
                   <td>*</td>
                   <td class="money">70 €</td>
                   <td>Tuesday, 7 June 2022 11:05:21 am Europe/Madrid</td>
@@ -134,11 +132,13 @@
                   <td class="completed">🤟</td>
                </tr>
                <tr>
-                  <td>Ricardo Ruiz</td>
+                  <td>Ricardo Ruiz Martínez</td>
+                  <td>44365676X</td>
                   <td>Cajasur Kutxabank</td>
                   <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
                   <td>*</td>
+                  <td>ING</td>
                   <td>*</td>
                   <td class="money">50 €</td>
                   <td>Tuesday, 7 June 2022 12:21:55 pm Europe/Madrid</td>
@@ -148,11 +148,13 @@
                   <td class="completed">🤟</td>
                </tr>
                <tr>
-                  <td>Ricardo Ruiz</td>
+                  <td>Ricardo Ruiz Martínez</td>
+                  <td>44365676X</td>
                   <td>Cajasur Kutxabank</td>
                   <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
                   <td>*</td>
+                  <td>ING</td>
                   <td>*</td>
                   <td class="money">230 €</td>
                   <td>Wednesday, 8 June 2022 8:18:10 am Europe/Madrid</td>
@@ -162,11 +164,13 @@
                   <td class="completed">🤟</td>
                </tr>
                <tr>
-                  <td>Ricardo Ruiz</td>
+                  <td>Ricardo Ruiz Martínez</td>
+                  <td>44365676X</td>
                   <td>Cajasur Kutxabank</td>
                   <td>ES92 0237 6057 8091 7267 1245</td>
-                  <td>Sergio Villarreal</td>
+                  <td>Sergio Villarreal Sáez</td>
                   <td>*</td>
+                  <td>BBVA</td>
                   <td>*</td>
                   <td class="money">100 €</td>
                   <td>Wednesday, 8 June 2022 10:02:12 am Europe/Madrid</td>
@@ -175,9 +179,43 @@
                   <td>GESTIONES CREACION EMPRESA</td>
                   <td class="completed">🤟</td>
                </tr>
+               <tr>
+                  <td>Ricardo Ruiz Martínez</td>
+                  <td>44365676X</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td>Jonathan Martínez</td>
+                  <td>*</td>
+                  <td>ING</td>
+                  <td>*</td>
+                  <td class="money">75 €</td>
+                  <td>Wednesday, 8 June 2022 17:14:05 pm Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>ONEDEVS.TECH TRIBAL MENOS SOBRECARGADO</td>
+                  <td class="completed">🤟</td>
+               </tr>
+               <tr>
+                  <td>Ricardo Ruiz Martínez</td>
+                  <td>44365676X</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td>Jonathan Martínez</td>
+                  <td>*</td>
+                  <td>ING</td>
+                  <td>*</td>
+                  <td class="money">25 €</td>
+                  <td>Wednesday, 8 June 2022 17:57:35 pm Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
+                  <td>ONEDEVS.TECH ONEDEVS.TECH LOGO CON F MAYUSCULA</td>
+                  <td class="completed">🤟</td>
+               </tr>
             </tbody>
          </table>
+
       </div>
+
    </body>
 
 </html>
