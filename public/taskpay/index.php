50c1a18a40a75d74c0b062e98a0658f29f104b74
webmaster <webmaster@onedevs.tech>
2022-06-08 10:05:47 +0200

    update task & pay

---
+++ public/taskpay/index.php
@@ -0,0 +1,176 @@
+<html>
+
+   <head>
+      <title>OneDevs Tasks & Pay</title>
+      <link rel="stylesheet" href="../css/styles.css?v=1.0">
+   </head>
+
+   <body class="money">
+
+      <div class="top">__onedevs__</div>
+      <div class="lema">Developing the Future</div>
+
+      <!-- DATE TIME -->
+
+      <div class="date">
+         <?= \date('l, j F Y G:i:s a e'); ?>
+      </div>
+
+      <div class="money">
+
+         <!-- AVAILABLE -->
+
+         <table class="available" align="center">
+            <thead>
+               <th>Available</th>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>3.753,52 €</td>
+               </tr>
+            </tbody>
+         </table>
+
+         <!-- OFFERS -->
+
+         <table class="offers" align="center">
+            <thead>
+               <tr>
+                  <th colspan="4" class="title">Work Offers</th>
+               </tr>
+               <tr>
+                  <th>Project</th>
+                  <th>From</th>
+                  <th>Offer</th>
+                  <th>Description</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
+                  <td>Ricardo Ruiz</td>
+                  <td class="money">175 €</td>
+                  <td>rediseñar y maquetar la página <a href="http://onedevs.tech/taskpay/" target="_blank">http://onedevs.tech/taskpay/</a> Ahora mismo está muy simplona, tiene que ser más llamativa y guay.<br />las filas se deben poder ordenar haciendo click en el nombre de la columna</td>
+               </tr>
+               <tr>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>Ricardo Ruiz</td>
+                  <td class="money">75 €</td>
+                  <td>cambiar el tribal de arriba, por uno que sea uno más fino, el que hay actualmente es un poco sobrecargado (debe seguir siendo blanco)</td>
+               </tr>
+            </tbody>
+         </table>
+
+         <!-- PAYMENTS -->
+
+         <table class="transactions" align="center">
+            <thead>
+               <tr>
+                  <th colspan="12" class="title">Payments</th>
+               </tr>
+               <tr>
+                  <th>From</th>
+                  <th>From Bank</th>
+                  <th>From IBAN</th>
+                  <th>To</th>
+                  <th>To Bank</th>
+                  <th>To IBAN</th>
+                  <th>Value</th>
+                  <th>Payment Date</th>
+                  <th>Payment Method</th>
+                  <th>Project</th>
+                  <th>Task / Payment Description</th>
+                  <th>Completed</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>Ricardo Ruiz</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td>Jonathan Martínez</td>
+                  <td>*</td>
+                  <td>*</td>
+                  <td class="money">75 €</td>
+                  <td>Monday, 6 June 2022 09:11:32 am Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>IMAGENES EGIPCIAS</td>
+                  <td class="completed">🤟</td>
+               </tr>
+               <tr>
+                  <td>Ricardo Ruiz</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td>Jonathan Martínez</td>
+                  <td>*</td>
+                  <td>*</td>
+                  <td class="money">125 €</td>
+                  <td>Monday, 6 June 2022 19:51:36 pm Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>MENU RESPONSIVE WWW.TATTOORUBIO.COM</td>
+                  <td class="completed">🤟</td>
+               </tr>
+               <tr>
+                  <td>Ricardo Ruiz</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td>Jonathan Martínez</td>
+                  <td>*</td>
+                  <td>*</td>
+                  <td class="money">70 €</td>
+                  <td>Tuesday, 7 June 2022 11:05:21 am Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>TRIBAL WWW.TATTOORUBIO.COM</td>
+                  <td class="completed">🤟</td>
+               </tr>
+               <tr>
+                  <td>Ricardo Ruiz</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td>Jonathan Martínez</td>
+                  <td>*</td>
+                  <td>*</td>
+                  <td class="money">50 €</td>
+                  <td>Tuesday, 7 June 2022 12:21:55 pm Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
+                  <td>WWW.TATTORUBIO.COM BANDERAS Y FAVICON</td>
+                  <td class="completed">🤟</td>
+               </tr>
+               <tr>
+                  <td>Ricardo Ruiz</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td>Jonathan Martínez</td>
+                  <td>*</td>
+                  <td>*</td>
+                  <td class="money">230 €</td>
+                  <td>Wednesday, 8 June 2022 8:18:10 am Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
+                  <td>ONEDEVS.TECH DISEÑO LOGO Y ADAPTACION A SVG</td>
+                  <td class="completed">🤟</td>
+               </tr>
+               <tr>
+                  <td>Ricardo Ruiz</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td>Sergio Villarreal</td>
+                  <td>*</td>
+                  <td>*</td>
+                  <td class="money">100 €</td>
+                  <td>Wednesday, 8 June 2022 10:02:12 am Europe/Madrid</td>
+                  <td>Wire Transfer</td>
+                  <td>Empresa</td>
+                  <td>GESTIONES CREACION EMPRESA</td>
+                  <td class="completed">🤟</td>
+               </tr>
+            </tbody>
+         </table>
+      </div>
+   </body>
+
+</html>
