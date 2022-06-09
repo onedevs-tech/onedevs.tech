a4aad7724219cd87ee5d73e50968ac2bf9b27e76
webmaster <webmaster@onedevs.tech>
2022-06-09 09:19:09 +0200

    eliminados los datos bancarios de taskpay, para que la info sea más clara, y preparación de la home de onedevs.tech

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
+<?php
+
+   function get_random_token ()
+   {
+      return rand(0, 9) .
+             rand(0, 9) .
+             rand(0, 9) .
+             rand(0, 9) .
+             rand(0, 9) .
+             rand(0, 9) .
+             rand(0, 9);
+   }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -10,9 +24,18 @@
       <meta property="og:type" content="website">
       <meta property="og:url" content="http://onedevs.tech">
       <meta property="og:description" content="OneDevs">
-      <link rel="stylesheet" href="css/styles.css?v=1.0">
+      <link rel="stylesheet" href="css/styles.css?v=<?= get_random_token() ?>">
+      <link rel="stylesheet" href="css/home.css?v=<?= get_random_token() ?>">
    </head>
    <body>
+      <div class="topmenu">
+         <ul>
+            <li>OneDevs</li>
+            <li>Quiénes somos</li>
+            <li>Qué hacemos</li>
+            <li>Dónde estamos</li>
+         </ul>
+      </div>
       <center>
          <img src="images/logo-solo-circulo.svg" width="200" alt="logo">
          <div class="nasalized" style="font-size: 1.2em;">__ OneDevs __</div>
--- public/taskpay/index.php
+++ public/taskpay/index.php
@@ -69,150 +69,96 @@
          <table class="transactions" align="center">
             <thead>
                <tr>
-                  <th colspan="14" class="title">Payments</th>
+                  <th colspan="8" class="title">Payments</th>
                </tr>
                <tr>
                   <th>From</th>
-                  <th>From Doc ID</th>
-                  <th>From Bank</th>
-                  <th>From IBAN</th>
                   <th>To</th>
-                  <th>To Doc ID</th>
-                  <th>To Bank</th>
-                  <th>To IBAN</th>
-                  <th>Value</th>
-                  <th>Payment Date</th>
+                  <th>Money</th>
                   <th>Payment Method</th>
+                  <th>Payment Date</th>
                   <th>Project</th>
                   <th>Task / Payment Description</th>
-                  <th>Completed</th>
+                  <th>Paid</th>
                </tr>
             </thead>
             <tbody>
                <tr>
                   <td>Ricardo Ruiz Martínez</td>
-                  <td>44365676X</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
-                  <td>*</td>
-                  <td>ING</td>
-                  <td>*</td>
                   <td class="money">75 €</td>
-                  <td>Monday, 6 June 2022 09:11:32 am Europe/Madrid</td>
                   <td>Wire Transfer</td>
+                  <td>Monday, 6 June 2022 09:11:32 am Europe/Madrid</td>
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td>IMAGENES EGIPCIAS</td>
                   <td class="completed">🤟</td>
                </tr>
                <tr>
                   <td>Ricardo Ruiz Martínez</td>
-                  <td>44365676X</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
-                  <td>*</td>
-                  <td>ING</td>
-                  <td>*</td>
                   <td class="money">125 €</td>
-                  <td>Monday, 6 June 2022 19:51:36 pm Europe/Madrid</td>
                   <td>Wire Transfer</td>
+                  <td>Monday, 6 June 2022 19:51:36 pm Europe/Madrid</td>
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td>MENU RESPONSIVE WWW.TATTOORUBIO.COM</td>
                   <td class="completed">🤟</td>
                </tr>
                <tr>
                   <td>Ricardo Ruiz Martínez</td>
-                  <td>44365676X</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
-                  <td>*</td>
-                  <td>ING</td>
-                  <td>*</td>
                   <td class="money">70 €</td>
-                  <td>Tuesday, 7 June 2022 11:05:21 am Europe/Madrid</td>
                   <td>Wire Transfer</td>
+                  <td>Tuesday, 7 June 2022 11:05:21 am Europe/Madrid</td>
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td>TRIBAL WWW.TATTOORUBIO.COM</td>
                   <td class="completed">🤟</td>
                </tr>
                <tr>
                   <td>Ricardo Ruiz Martínez</td>
-                  <td>44365676X</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
-                  <td>*</td>
-                  <td>ING</td>
-                  <td>*</td>
                   <td class="money">50 €</td>
-                  <td>Tuesday, 7 June 2022 12:21:55 pm Europe/Madrid</td>
                   <td>Wire Transfer</td>
+                  <td>Tuesday, 7 June 2022 12:21:55 pm Europe/Madrid</td>
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td>WWW.TATTORUBIO.COM BANDERAS Y FAVICON</td>
                   <td class="completed">🤟</td>
                </tr>
                <tr>
                   <td>Ricardo Ruiz Martínez</td>
-                  <td>44365676X</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
-                  <td>*</td>
-                  <td>ING</td>
-                  <td>*</td>
                   <td class="money">230 €</td>
-                  <td>Wednesday, 8 June 2022 8:18:10 am Europe/Madrid</td>
                   <td>Wire Transfer</td>
+                  <td>Wednesday, 8 June 2022 8:18:10 am Europe/Madrid</td>
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td>ONEDEVS.TECH DISEÑO LOGO Y ADAPTACION A SVG</td>
                   <td class="completed">🤟</td>
                </tr>
                <tr>
                   <td>Ricardo Ruiz Martínez</td>
-                  <td>44365676X</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Sergio Villarreal Sáez</td>
-                  <td>*</td>
-                  <td>BBVA</td>
-                  <td>*</td>
                   <td class="money">100 €</td>
-                  <td>Wednesday, 8 June 2022 10:02:12 am Europe/Madrid</td>
                   <td>Wire Transfer</td>
-                  <td>Empresa</td>
+                  <td>Wednesday, 8 June 2022 10:02:12 am Europe/Madrid</td>
+                  <td>OneDevs Digital Services S.L.</td>
                   <td>GESTIONES CREACION EMPRESA</td>
                   <td class="completed">🤟</td>
                </tr>
                <tr>
                   <td>Ricardo Ruiz Martínez</td>
-                  <td>44365676X</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
-                  <td>*</td>
-                  <td>ING</td>
-                  <td>*</td>
                   <td class="money">75 €</td>
-                  <td>Wednesday, 8 June 2022 17:14:05 pm Europe/Madrid</td>
                   <td>Wire Transfer</td>
+                  <td>Wednesday, 8 June 2022 17:14:05 pm Europe/Madrid</td>
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td>ONEDEVS.TECH TRIBAL MENOS SOBRECARGADO</td>
                   <td class="completed">🤟</td>
                </tr>
                <tr>
                   <td>Ricardo Ruiz Martínez</td>
-                  <td>44365676X</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
                   <td>Jonathan Martínez</td>
-                  <td>*</td>
-                  <td>ING</td>
-                  <td>*</td>
                   <td class="money">25 €</td>
-                  <td>Wednesday, 8 June 2022 17:57:35 pm Europe/Madrid</td>
                   <td>Wire Transfer</td>
+                  <td>Wednesday, 8 June 2022 17:57:35 pm Europe/Madrid</td>
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td>ONEDEVS.TECH ONEDEVS.TECH LOGO CON F MAYUSCULA</td>
                   <td class="completed">🤟</td>
