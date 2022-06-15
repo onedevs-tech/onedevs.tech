17017afd50cac1823f8ca2b2ceb907bf40542ccb
webmaster <webmaster@onedevs.tech>
2022-06-15 17:35:53 +0200

    update task & pay

--- public/taskpay/index.php
+++ public/taskpay/index.php
@@ -8,75 +8,41 @@
    include $src_dir . 'widgets/onedevs_favicons.php';
    include $src_dir . 'widgets/onedevs_logo.php';
 
-   $now_unixtime = \time();
-   $now_date = \date('Y-m-j * l', $now_unixtime);
-   $now_time = \date('G:i:s a', $now_unixtime);
-   $now_zone = \date('e', $now_unixtime);
-
 ?>
 
 <html lang="es">
 
    <head>
-      <title>🤘 🤑 Task & Pay - - <?= onedevs_title() ?></title>
+      <title>🤑 Task & Pay</title>
       <?= onedevs_favicons('security') ?>
       <meta name="msapplication-TileColor" content="#ffffff">
       <meta name="theme-color" content="#ffffff">
       <link rel="stylesheet" href="../css/vars.css?v=<?= random_numeric_token() ?>">
       <link rel="stylesheet" href="../css/fonts.css?v=<?= random_numeric_token() ?>">
       <link rel="stylesheet" href="../css/generic.css?v=<?= random_numeric_token() ?>">
-      <link rel="stylesheet" href="../css/task&pay.css?v=<?= random_numeric_token() ?>">
+      <link rel="stylesheet" href="../css/taskpay.css?v=<?= random_numeric_token() ?>">
    </head>
 
-   <body class="money">
+   <body>
 
       <center>
          <div class="app">
             Task & Pay
-            <div class="datetime">
-               <?= $now_date ?><br />
-               <?= $now_time ?><br />
-               <?= $now_zone ?><br />
-            </div>
          </div>
       </center>
 
       <div class="taskpay">
 
-         <!-- AVAILABLE -->
-
-         <table class="table available" align="center">
-            <thead>
-               <tr>
-                  <th colspan="4" class="title">Available</th>
-               </tr>
-               <tr>
-                  <th>Bank Country</th>
-                  <th>Bank</th>
-                  <th>IBAN</th>
-                  <th>✌🤑</th>
-               </tr>
-            </thead>
-            <tbody>
-               <tr>
-                  <td>Spain</td>
-                  <td>Cajasur Kutxabank</td>
-                  <td>ES92 0237 6057 8091 7267 1245</td>
-                  <td class="money">1.825,66 €</td>
-               </tr>
-            </tbody>
-         </table>
-
-         <!-- TASKS -->
+         <!-- TASK -->
 
          <table class="table tasks" align="center">
             <thead>
                <tr>
-                  <th colspan="4" class="title">Tasks</th>
+                  <th colspan="4" class="title">Task</th>
                </tr>
                <tr>
                   <th>Bidder</th>
-                  <th>Reward</th>
+                  <th>🤑</th>
                   <th>Project</th>
                   <th>Task Description</th>
                </tr>
@@ -96,7 +62,7 @@
          <table class="table payments" align="center">
             <thead>
                <tr>
-                  <th colspan="8" class="title">Payments</th>
+                  <th colspan="8" class="title">Pay</th>
                </tr>
                <tr>
                   <th>Payer</th>
@@ -114,7 +80,7 @@
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td class="banking-concept">IMAGENES EGIPCIAS</td>
                   <td class="payment money nowrap align-right" title="Monday, 6 June 2022 09:11:32 am Europe/Madrid">75,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -122,7 +88,7 @@
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td class="banking-concept">MENU RESPONSIVE WWW.TATTOORUBIO.COM</td>
                   <td class="payment money nowrap align-right" title="Monday, 6 June 2022 19:51:36 pm Europe/Madrid">125,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -130,7 +96,7 @@
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td class="banking-concept">TRIBAL WWW.TATTOORUBIO.COM</td>
                   <td class="payment money nowrap align-right" title="Tuesday, 7 June 2022 11:05:21 am Europe/Madrid">70,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -138,7 +104,7 @@
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td class="banking-concept">WWW.TATTORUBIO.COM BANDERAS Y FAVICON</td>
                   <td class="payment money nowrap align-right" title="Tuesday, 7 June 2022 12:21:55 pm Europe/Madrid">50,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -146,7 +112,7 @@
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td class="banking-concept">ONEDEVS.TECH DISEÑO LOGO Y ADAPTACION A SVG</td>
                   <td class="payment money nowrap align-right" title="Wednesday, 8 June 2022 8:18:10 am Europe/Madrid">230,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -154,7 +120,7 @@
                   <td>Enterprise</td>
                   <td class="banking-concept">GESTIONES CREACION EMPRESA</td>
                   <td class="payment money nowrap align-right" title="Wednesday, 8 June 2022 10:02:12 am Europe/Madrid">100,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -162,7 +128,7 @@
                   <td><a href="https://www.tattoorubio.com" target="_blank">www.tattoorubio.com</a></td>
                   <td class="banking-concept">ONEDEVS.TECH TRIBAL MENOS SOBRECARGADO</td>
                   <td class="payment money nowrap align-right" title="Wednesday, 8 June 2022 17:14:05 pm Europe/Madrid">75,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -170,7 +136,7 @@
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td class="banking-concept">ONEDEVS.TECH ONEDEVS.TECH LOGO CON F MAYUSCULA</td>
                   <td class="payment money nowrap align-right" title="Wednesday, 8 June 2022 17:57:35 pm Europe/Madrid">25,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -178,7 +144,7 @@
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td class="banking-concept">ONEDEVS.TECH REMAQUETAR TASKPAY Y FAVICON</td>
                   <td class="payment money nowrap align-right" title="Friday, 10 June 2022 10:45:15 am Europe/Madrid">200,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -186,7 +152,7 @@
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td class="banking-concept">SONIDO MAS HOVER</td>
                   <td class="payment money nowrap align-right" title="Saturday, 11 June 2022 10:06:59 am Europe/Madrid">200,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -194,7 +160,7 @@
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td class="banking-concept">RESPONSIVE HOME ONEDEVS + SONIDO VIDEOS YOUTUBE</td>
                   <td class="payment money nowrap align-right" title="Saturday, 14 June 2022 8:41:42 am Europe/Madrid">530,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
                </tr>
                <tr>
                   <td class="nowrap">richi</td>
@@ -202,7 +168,49 @@
                   <td><a href="https://onedevs.tech" target="_blank">onedevs.tech</a></td>
                   <td class="banking-concept">JAVASCRIPT SOUNDCLOUD</td>
                   <td class="payment money nowrap align-right" title="Tuesday, 14 June 2022 11:21:30 am Europe/Madrid">215,00 €</td>
-                  <td>✌</td>
+                  <td>👍</td>
+               </tr>
+            </tbody>
+         </table>
+
+         <!-- AVAILABLE -->
+
+         <?php
+
+            $now_unixtime = \time();
+            $now_date = \date('l d F Y', $now_unixtime);
+            $now_time = \date('G:i:s a', $now_unixtime);
+            $now_zone = \date('e', $now_unixtime);
+
+         ?>
+
+         <table class="table available" align="center">
+            <thead>
+               <tr>
+                  <th colspan="4" class="title">
+                     Available Now
+                     <div class="datetime">
+                        ...
+                        <?= $now_date ?>
+                        <?= $now_time ?>
+                        <?= $now_zone ?>
+                        ...
+                     </div>
+                  </th>
+               </tr>
+               <tr>
+                  <th>Country</th>
+                  <th>Bank</th>
+                  <th>IBAN</th>
+                  <th>🤑</th>
+               </tr>
+            </thead>
+            <tbody>
+               <tr>
+                  <td>Spain</td>
+                  <td>Cajasur Kutxabank</td>
+                  <td>ES92 0237 6057 8091 7267 1245</td>
+                  <td class="money">1.825,66 €</td>
                </tr>
             </tbody>
          </table>
@@ -215,8 +223,8 @@
                   <th colspan="2" class="title">Top</th>
                </tr>
                <tr>
-                  <th>Crack</th>
-                  <th>✌🤑</th>
+                  <th>Top</th>
+                  <th>🤑</th>
                </tr>
             </thead>
             <tbody>
