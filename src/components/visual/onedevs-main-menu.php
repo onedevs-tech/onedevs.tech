df00ce976024974b7ca85c04e0f22c7dd470a3be
webmaster <webmaster@onedevs.tech>
2022-06-17 08:02:41 +0200

    fix menú stack tecnológico

--- src/components/visual/onedevs-main-menu.php
+++ src/components/visual/onedevs-main-menu.php
@@ -9,7 +9,7 @@
       <div class="onedevs-main-menu">
          <div class="item"><a href="#quienes-somos">Quiénes Somos</a></div>
          <div class="item"><a href="#que-hacemos">Qué Hacemos</a></div>
-         <div class="item"><a href="#de-que-controlamos">De Qué Controlamos</a></div>
+         <div class="item"><a href="#stack-tecnologico">De Qué Controlamos</a></div>
          <div class="item"><a href="#portfolio">Confía En OneDevs</a></div>
          <div class="item"><a href="#location">Dónde Estamos</a></div>
          <div class="item"><a href="blog/">Blog OneDevs</a></div>
