8e7ce945ead0f744ae2e1ee64585a4609fe6ccd9
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-06-29 19:48:42 +0200

    z_deploy_production.sh

---
+++ z_deploy_production.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd /var/www/onedevs.tech
+sudo git pull origin main
+apubu permissions onedevs.tech
+echo ""
+echo NO OLVIDES MIRAR EL .env
+echo ""
--- z_set_env_production.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-echo "ENVIRONMENT=production" > ./.env
