83edd25faffbfb401737a5410bfec5b7746d27f4
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-07-02 05:09:16 +0200

    comments

--- public/404.php
+++ public/404.php
@@ -1,7 +1,11 @@
 <?php
 
+   // inicialización
+   //
    $config_filepath = __DIR__ . '/../.config';
    include_once '/var/www/shared/init/init.php';
    include_once '/var/www/shared/sec/src/exec/sec_register_http_request.php';
 
+   // vista
+   //
    include_once $GLOBALS['config']['project_root_dir'] . '/phtml/page/404.phtml';
--- public/home.php
+++ public/home.php
@@ -1,6 +1,10 @@
 <?php
 
+   // inicialización
+   //
    $config_filepath = __DIR__ . '/../.config';
    include_once '/var/www/shared/init/init.php';
 
+   // vista
+   //
    include_once $GLOBALS['config']['project_root_dir'] . '/phtml/page/home.phtml';
