b71447445843a6b4cfd975b48ef0290bf613830b
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-07-04 09:15:44 +0200

    ajustes

---
+++ public/js/component/cookies.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+   $('.btn-aceptar-cookies').click(function(){
+      $('.cookies').addClass('d-none');
+   });
+
+   $('.btn-rechazar-cookies').click(function(){
+      $('.cookies').addClass('d-none');
+   });
+
+});
