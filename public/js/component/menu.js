d56144e8464902d29453ea2f4c5c1b5d15a0b86c
Ricardo Ruiz Martínez <richiruizmartinez@gmail.com>
2022-07-09 10:49:48 +0200

    mejoras en el javascript del menú

--- public/js/component/menu.js
+++ public/js/component/menu.js
@@ -1,20 +1,33 @@
 
+
 $(document).ready(function(){
 
    $('.menu .control.open').click(function(){
-
-      $('.menu .control.open').addClass('d-none');
-      $('.menu .control.close').removeClass('d-none');
-      $('.menu div.options').removeClass('d-none');
-
+      open_menu();
    });
 
    $('.menu .control.close').click(function(){
+      close_menu();
+   });
 
-      $('.menu .control.close').addClass('d-none');
-      $('.menu .control.open').removeClass('d-none');
-      $('.menu div.options').addClass('d-none');
-
+   $('.menu ul.options a').click(function(){
+      close_menu();
    });
 
 });
+
+
+function open_menu()
+{
+   $('.menu .control.open').addClass('d-none');
+   $('.menu .control.close').removeClass('d-none');
+   $('.menu div.options').removeClass('d-none');
+}
+
+
+function close_menu()
+{
+   $('.menu .control.close').addClass('d-none');
+   $('.menu .control.open').removeClass('d-none');
+   $('.menu div.options').addClass('d-none');
+}
